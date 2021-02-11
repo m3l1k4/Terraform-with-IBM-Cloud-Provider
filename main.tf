b2ffa9001445071ad43e56b339d46913e14bf7a3
m3l1k4 <melikaihelas@gmail.com>
2021-02-10 21:17:18 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,19 @@
+terraform {
+  required_providers {
+    ibm = {
+      source  = "localdomain/provider/ibm"
+      version = "1.20.0"
+    }
+  }
+}
+
+variable ibmcloud_api_key {
+}
+
 provider "ibm" {
-  generation         = 2
-  region             = "us-south"
+  ibmcloud_api_key = var.ibmcloud_api_key
 }
 
-data "ibm_iam_access_group" "accgroup" {
-}
-
-output "accgroups" {
-  value = data.ibm_iam_access_group.accgroup.groups[*].name
+resource ibm_resource_group new_group {
+  name = "created-by-terraform"
 }
